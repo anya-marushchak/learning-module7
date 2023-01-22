3492d05c8f4509a9f2aaf1e66f4a0a8099b009b6
Anna Marushchak <maruschakanya86@gmail.com>
2023-01-22 18:46:41 +0100

    Import Faker

--- exercise_3.py
+++ exercise_3.py
@@ -7,18 +7,28 @@ class BusinessCard:
 
 
 # creating list
+
 x_1 = BusinessCard(name = "Raymond J.", surname = "White", e_mail = "RaymondJWhite@armyspy.com")
 x_2 = BusinessCard(name = "David L.", surname = "Riemer", e_mail = "DavidLRiemer@dayrep.com")
 x_3 = BusinessCard(name = "George V.", surname = "Glisson", e_mail = "GeorgeVGlisson@rhyta.com")
-x = [x_1, x_2, x_3]
+x = [x_1,x_2,x_3]
 
+def __str__(self):
+    return f'{self.name} {self.surname} {self.e_mail}'
+print(x)
 
-by_name = sorted(x, key=lambda BusinessCard: BusinessCard.name)
-by_surname = sorted(x, key=lambda BusinessCard: BusinessCard.surname)
-by_e_mail = sorted(x, key=lambda BusinessCard: BusinessCard.e_mail)
-
-lambda BusinessCard: BusinessCard.name
+by_name = sorted(x, key= lambda x: x.name)
 print(by_name)
+
+by_surname = sorted(x, key=lambda x: x.surname)
+by_e_mail = sorted(x, key=lambda x: x.e_mail)
+
+
+
+
+
+
+
 
 
 
