34d3f3f762931ea3659724704692e96d0c904eb8
Anna Marushchak <maruschakanya86@gmail.com>
2023-01-23 09:23:15 +0100

    Edit file

--- exercise_3.py
+++ exercise_3.py
@@ -7,21 +7,18 @@ class BusinessCard:
 
 
 # creating list
+BusinessCard_1 = BusinessCard(name = "Raymond J.", surname = "White", e_mail = "RaymondJWhite@armyspy.com")
+BusinessCard_2 = BusinessCard(name = "David L.", surname = "Riemer", e_mail = "DavidLRiemer@dayrep.com")
+BusinessCard_3 = BusinessCard(name = "George V.", surname = "Glisson", e_mail = "GeorgeVGlisson@rhyta.com")
+BusinessCards = [BusinessCard_1, BusinessCard_2, BusinessCard_3]
 
-x_1 = BusinessCard(name = "Raymond J.", surname = "White", e_mail = "RaymondJWhite@armyspy.com")
-x_2 = BusinessCard(name = "David L.", surname = "Riemer", e_mail = "DavidLRiemer@dayrep.com")
-x_3 = BusinessCard(name = "George V.", surname = "Glisson", e_mail = "GeorgeVGlisson@rhyta.com")
-x = [x_1,x_2,x_3]
 
-def __str__(self):
-    return f'{self.name} {self.surname} {self.e_mail}'
-print(x)
+by_name = sorted(BusinessCards, key=lambda BusinessCard: BusinessCard.name)
+by_surname = sorted(BusinessCards, key=lambda BusinessCard: BusinessCard.surname)
+by_e_mail = sorted(BusinessCards, key=lambda BusinessCard: BusinessCard.e_mail)
 
-by_name = sorted(x, key= lambda x: x.name)
+lambda BusinessCards: BusinessCards.name
 print(by_name)
-
-by_surname = sorted(x, key=lambda x: x.surname)
-by_e_mail = sorted(x, key=lambda x: x.e_mail)
 
 
 
