8367b7b928d5319418aa9126fc6989c8d4b151d4
Anna Marushchak <maruschakanya86@gmail.com>
2023-01-24 11:49:30 +0100

    Edit file

--- businesscard.py
+++ businesscard.py
@@ -1,16 +1,26 @@
 #creating class
 class BaseContact:
-    def  __init__ (self, name, surname, phone_number, e_mail):
+    def  __init__ (self, name, surname, phone_number, e_mail, type, quantity):
         self.name = name
         self.surname = surname
         self.phone_number = phone_number
         self.e_mail = e_mail
+        self.type = type
+        self.quantity = quantity
 
 
+    # creating @property decorator for BaseContact
     @property
     def name_size(self):
         length = self.name +self.surname
         return len(length)
+
+    # creating method to compare type and quantity attributes
+    def __eq__(self, other):
+        return (
+            self.type == other.type and
+            self.quantity == other.quantity 
+        )
 
 #creating subclass
 class BusinessContact(BaseContact):
@@ -20,7 +30,7 @@ class BusinessContact(BaseContact):
         self.company_name = company_name
         self.company_number = company_number
 
-
+    # creating @property decorator for BusinessContact
     @property
     def name_size(self):
         length = self.name +self.surname
@@ -29,12 +39,13 @@ class BusinessContact(BaseContact):
 
 
 # creating list to class BaseContact
-list_1 = BaseContact(name = "Raymond J.", surname = "White",phone_number = "+48589589525",e_mail = "RaymondJWhite@armyspy.com" )
+list_1 = BaseContact(name = "Raymond J.", surname = "White",phone_number = "+48589589525",e_mail = "RaymondJWhite@armyspy.com" ,type = "base contact", quantity = 1 )
 
 
 # creating list to class BusinessContact
-list_2 = BusinessContact(name ="Raymond J.",surname="White",phone_number="+48589589525",e_mail = "RaymondJWhite@armyspy.com", company_name = "Laura Ashley Mother & Child",position = "Administrative lead",company_number = "+48789252123")
+list_2 = BusinessContact(name ="Raymond J.",surname="White",phone_number="+48589589525",e_mail = "RaymondJWhite@armyspy.com",type = "business contact" ,company_name = "Laura Ashley Mother & Child",position = "Administrative lead",company_number = "+48789252123" , quantity = 1)
 
+list_1 == list_2
 
 # defining function to choose the neccessary card
 def contact():
