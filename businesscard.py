57a7e2d810dc26a55685b4013812f07c0affb64e
Anna Marushchak <maruschakanya86@gmail.com>
2023-01-24 10:56:09 +0100

    Edit file

--- businesscard.py
+++ businesscard.py
@@ -1,12 +1,16 @@
 #creating class
 class BaseContact:
-    def  __init__ (self, name, surname, phone_number, e_mail,length):
+    def  __init__ (self, name, surname, phone_number, e_mail):
         self.name = name
         self.surname = surname
         self.phone_number = phone_number
         self.e_mail = e_mail
-        self.length = length
 
+
+    @property
+    def name_size(self):
+        length = self.name +self.surname
+        return len(length)
 
 #creating subclass
 class BusinessContact(BaseContact):
@@ -17,12 +21,19 @@ class BusinessContact(BaseContact):
         self.company_number = company_number
 
 
+    @property
+    def name_size(self):
+        length = self.name +self.surname
+        return len(length)
+    
+
+
 # creating list to class BaseContact
-list_1 = BaseContact(name = "Raymond J.", surname = "White",phone_number = "+48589589525",e_mail = "RaymondJWhite@armyspy.com" , length = 0)
+list_1 = BaseContact(name = "Raymond J.", surname = "White",phone_number = "+48589589525",e_mail = "RaymondJWhite@armyspy.com" )
 
 
 # creating list to class BusinessContact
-list_2 = BusinessContact(name ="Raymond J.",surname="White",phone_number="+48589589525",e_mail = "RaymondJWhite@armyspy.com", length = 0, company_name = "Laura Ashley Mother & Child",position = "Administrative lead",company_number = "+48789252123")
+list_2 = BusinessContact(name ="Raymond J.",surname="White",phone_number="+48589589525",e_mail = "RaymondJWhite@armyspy.com", company_name = "Laura Ashley Mother & Child",position = "Administrative lead",company_number = "+48789252123")
 
 
 # defining function to choose the neccessary card
@@ -31,18 +42,12 @@ def contact():
         action = input ("Choose card: Basecontact or Businesscontact: ")
         if action == "Basecontact":
             
-                print(f'I am connecting to {list_1.name} {list_1.surname} number {list_1.phone_number} .')
-
+                print(f'I am connecting to {list_1.name} {list_1.surname} with the length of {list_1.name_size} words in name and surname, number {list_1.phone_number} .')
+ 
         if action == "Businesscontact":
             
-                print(f'I am connecting to {list_2.name} {list_2.surname} number {list_2.company_number} .')
+                print(f'I am connecting to {list_2.name} {list_2.surname} with the length of {list_2.name_size} words in name and surname, number {list_2.company_number}.')
 
-
-def label_length():
-    length = list_1.name + list_1.surname 
-    return (len(length))
-
-label_length()
 
 contact()
 
--- exercise_3.py
+++ exercise_3.py
@@ -4,25 +4,36 @@ class BusinessCard:
         self.name = name
         self.surname = surname
         self.e_mail = e_mail
+    
+    def __str__ (self):
+        return self.name 
 
+    def __repr__ (self):
+        return self.name
+
+    def __str__ (self):
+        return self.surname
+
+    def __repr__ (self):
+        return self.surname
 
 # creating list
-BusinessCard_1 = BusinessCard(name = "Raymond J.", surname = "White", e_mail = "RaymondJWhite@armyspy.com")
-BusinessCard_2 = BusinessCard(name = "David L.", surname = "Riemer", e_mail = "DavidLRiemer@dayrep.com")
-BusinessCard_3 = BusinessCard(name = "George V.", surname = "Glisson", e_mail = "GeorgeVGlisson@rhyta.com")
-BusinessCards = [BusinessCard_1, BusinessCard_2, BusinessCard_3]
+businessCard_1 = BusinessCard(name = "Raymond J.", surname = "White", e_mail = "RaymondJWhite@armyspy.com")
+businessCard_2 = BusinessCard(name = "David L.", surname = "Riemer", e_mail = "DavidLRiemer@dayrep.com")
+businessCard_3 = BusinessCard(name = "George V.", surname = "Glisson", e_mail = "GeorgeVGlisson@rhyta.com")
+businessCards = [businessCard_1, businessCard_2, businessCard_3]
 
 
-by_name = sorted(BusinessCards, key=lambda BusinessCard: BusinessCard.name)
-by_surname = sorted(BusinessCards, key=lambda BusinessCard: BusinessCard.surname)
-by_e_mail = sorted(BusinessCards, key=lambda BusinessCard: BusinessCard.e_mail)
-
-lambda BusinessCards: BusinessCards.name
-print(by_name)
+by_name = sorted(businessCards, key=lambda x: x.name)
+by_surname = sorted(businessCards, key=lambda x: x.surname)
+by_e_mail = sorted(businessCards, key=lambda x: x.e_mail)
 
 
-
-
+for card in by_name:
+    print (card) 
+    
+for card in by_surname:
+    print (card) 
 
 
 
